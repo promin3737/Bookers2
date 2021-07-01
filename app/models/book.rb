acf5db1684d5e13b6c8c368098d909e70d408271
promin3737 <s.s.hayabusa3737@icloud.com>
2021-07-01 14:35:45 +0000

    deploy for heroku

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :user
-  attachment :image
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200}
   has_many :book_comments, dependent: :destroy
